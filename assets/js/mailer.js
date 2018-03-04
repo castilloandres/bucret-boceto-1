f7f40a7c63385f4d4cb337d5c5c2073f91cab7be
Andres Castillo <castilloandres17@gmail.com>
2018-03-04 21:45:50 +0100

    Improvements

---
+++ assets/js/mailer.js
@@ -0,0 +1,59 @@
+$(function() {
+  console.log('mailer.js loaded!');
+  // Get the form.
+  var form = $('#ajax-contact');
+
+  // Get the messages div.
+  var formMessages = $('#form-messages');
+  console.log(form);
+  // Set up an event listener for the contact form.
+  $(form).submit(function(e) {
+    console.log('prevent defualt function');
+    // Stop the browser from submitting the form.
+    e.preventDefault();
+
+    // Serialize the form data.
+    //var formData = $(form).serialize();
+    console.log("JS form being serialized")
+     var formData = $(form).serialize();
+    //console.log(formData)
+        // Submit the form using AJAX.
+    $.ajax({
+      type: 'POST',
+      url: $(form).attr('action'),
+      data: formData,
+      dataType:'HTML'
+
+    })
+    .done(function(response) {
+      // Make sure that the formMessages div has the 'success' class.
+      $(formMessages).removeClass('error');
+      $(formMessages).addClass('success');
+
+      // Set the message text.
+      $(formMessages).text(response);
+      console.log("Succesful AJAX")
+
+      // Clear the form.
+      $('#name').val('');
+      $('#email').val('');
+      $('#message').val('');
+    })
+    .fail(function(data) {
+      // Make sure that the formMessages div has the 'error' class.
+      $(formMessages).removeClass('success');
+      $(formMessages).addClass('error');
+      console.log (data.responseText)
+      console.log("FAILED AJAX")
+
+      // Set the message text.
+      if (data.responseText !== '') {
+        $(formMessages).text(data.responseText);
+      } else {
+        $(formMessages).text('Oops! Ha ocurrido un error y su mensaje no ha sido enviado.');
+      }
+    });
+
+  });
+
+});
